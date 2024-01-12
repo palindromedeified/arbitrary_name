a432227a71e5fd27ca3365824f7475a291863466
andrey <holanpolain090@gmail.com>
2024-01-12 15:25:10 +0300

    eefefefefe

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import random
+
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
 from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor, QBrush
 from PyQt5.QtCore import Qt
 from PyQt5 import uic
 from random import randint
 import sys
+from UI import Ui_MainWindow
 
 
-class Test(QMainWindow):
+class Test(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
+        # uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.circle)
         canvas = QPixmap(600, 600)
         self.label.setPixmap(canvas)
@@ -20,9 +24,10 @@ class Test(QMainWindow):
         painter = QPainter(self.label.pixmap())
         pen = QPen()
         pen.setWidth(3)
-        pen.setColor(QColor(Qt.yellow))
+        color = QColor(*[random.randint(0, 256) for _ in range(3)])
+        pen.setColor(color)
         painter.setPen(pen)
-        painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+        painter.setBrush(QBrush(color, Qt.SolidPattern))
         painter.drawEllipse(x, y, radius, radius)
         painter.end()
         self.update()
